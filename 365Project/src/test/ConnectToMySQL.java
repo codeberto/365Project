4c71dabba2c17432f3fa5104fa63de1ef03c0160
MattS8 <Matthew.steinhardt@gmail.com>
2017-03-14 21:46:50 -0700

    Fixed issue with connecting to database while outside campus Wifi
    - NOTE: You need to add mysql-connector-java-5.1.36.jar to the libraries
    - NOTE: Make sure to supply your cal poly username and password to the two
    fields at the top of the Class

--- 365Project/src/test/ConnectToMySQL.java
+++ 365Project/src/test/ConnectToMySQL.java
@@ -6,57 +6,87 @@
 package test;
 
 import java.sql.*;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import java.util.Properties;
 
-/**
- *
- * @author cignx
- */
-public class ConnectToMySQL {
-   static Connection connect;
-   public static void runConnection(){
-       try {
-           Class.forName("com.mysql.jdbc.Driver");
-           connect = DriverManager.getConnection("jdbc:mysql://cslvm74.csc.calpoly.edu:3306/becarter?user=becarter&password=3x5Dipl4caiT");
-           connect.setAutoCommit(false);
-       } catch (Exception e) {
-           System.out.println("connection prob: " + e.getMessage());
-       }
-           
-   }
-   
-   public static void testQueryConnection(){
-       ResultSet rs;
-       Statement statement;
-       String workoutName;
-            
-       try{
-            statement = connect.createStatement();
-       
-            rs = statement.executeQuery("SELECT * FROM Workouts");
-            
-            while(rs.next()) {
-                workoutName = rs.getString(2);      // use column number
-                System.out.println("Username = " + workoutName);
-            }
-            
-       } catch (Exception e) {
-            System.out.println("query prob: " + e.getMessage());
-       }
-       
-       
-       
-       
-   }
-   
-   public void testInsertConnection(){
-       
-   }
-   
-   public static void closeConnection(){
-        try{
-            connect.close();
-        } catch (Exception e) {
-            System.out.println("close prob: " + e.getMessage());
+public class ConnectToMySQL {          
+    static Connection connect;
+    static final String strSshPassword = "YOUR_CAL_POLY_PASSWORD";                      // SSH login password
+    static final String strSshUser = "YOUR_CAL_POLY_USERNAME";                                // SSH loging username
+    
+    private static void doSshTunnel(String strSshUser, String strSshPassword, String strSshHost, int nSshPort,
+            String strRemoteHost, int nLocalPort, int nRemotePort) throws JSchException {
+        final JSch jsch = new JSch();
+        Session session = jsch.getSession(strSshUser, strSshHost, 22);
+        session.setPassword(strSshPassword);
+
+        final Properties config = new Properties();
+        config.put("StrictHostKeyChecking", "no");
+        session.setConfig(config);
+
+        session.connect();
+        session.setPortForwardingL(nLocalPort, strRemoteHost, nRemotePort);
+    }
+    
+    public static void runConnection() {
+        try {
+            String strSshHost = "unix12.csc.calpoly.edu";                       // hostname or ip or SSH server
+            int nSshPort = 22;                                                  // remote SSH host port number
+            String strRemoteHost = "cslvm74.csc.calpoly.edu";                   // hostname
+            int nLocalPort = 3366;                                              // local port number use to bind SSH tunnel
+            int nRemotePort = 3306;                                             // remote port number of your database
+            String strDbUser = "becarter";                                      // database loging username
+            String strDbPassword = "3x5Dipl4caiT";                              // database login password
+
+            ConnectToMySQL.doSshTunnel(strSshUser, strSshPassword, strSshHost, nSshPort, strRemoteHost, nLocalPort,
+                    nRemotePort);
+
+            Class.forName("com.mysql.jdbc.Driver");
+            connect = DriverManager.getConnection("jdbc:mysql://localhost:" + nLocalPort, strDbUser,
+                       strDbPassword);
+            connect.setCatalog(strDbUser);
+            connect.setAutoCommit(false);
+        } catch(Exception e) {
+            System.out.println("connection prob: " + e.toString());
         }
-   }
+       
+    }
+
+    public static void testQueryConnection(){
+        ResultSet rs;
+        Statement statement;
+        String workoutName;
+
+        try{
+             statement = connect.createStatement();
+
+             rs = statement.executeQuery("SELECT * FROM Workouts");
+
+             while(rs.next()) {
+                 workoutName = rs.getString(2);      // use column number
+                 System.out.println("Username = " + workoutName);
+             }
+
+        } catch (Exception e) {
+             System.out.println("query prob: " + e.getMessage());
+        }
+
+
+
+
+    }
+
+    public void testInsertConnection(){
+
+    }
+
+    public static void closeConnection(){
+         try{
+             connect.close();
+         } catch (Exception e) {
+             System.out.println("close prob: " + e.getMessage());
+         }
+    }
 }
